2d395dfab1712032b67bbe18a261b44994d94cb1
federicoScremin <federicoScremin@users.noreply.github.com>
2018-11-24 11:03:52 +0100

    Add files via upload
    Creazione applicazione di base con:
    - popolamento db (simulatoreSensore.py)
    - Backend --> NodeJS + MongoDB (API Rest)
    - server.js --> connessione al database MongoDB in localhost, routes
    - visualizzazione del contenuto della collection "temperatures" nella route "localhost:8080/api/temperatures"

---
+++ server.js
@@ -0,0 +1,90 @@
+// server.js
+
+// set up
+var express = require('express');
+var app = express(); // create our app w/ express
+var mongoose = require('mongoose'); // mongoose for mongodb
+var morgan = require('morgan'); // log requests to the console (express4)
+var bodyParser = require('body-parser'); // pull information from HTML POST (express4)
+var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+
+// configuration and connection to mongoDB database
+mongoose.connect('mongodb://localhost:27017/zero12_db', {useNewUrlParser: true}).then(() => {
+    console.log("Successfully connected to the database");    
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
+
+app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
+app.use(morgan('dev')); // log every request to the console
+app.use(bodyParser.urlencoded({'extended':'true'})); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(methodOverride());
+
+
+// define model
+var Temperature = mongoose.model('Temperature', {
+    _id: String,
+    Value: String,
+    Timestamp: String
+});
+
+
+// ROUTES API
+// get all temperatures
+app.get('/api/temperatures', function(req, res) {
+    // use mongoose to get all temperatures in the database
+    Temperature.find(function(err, temperatures) {
+        // if there is an error retrieving, send the error. nothing after res.send(err) will execute
+        if (err)
+            res.send(err)
+        res.json(temperatures); // return all todos in JSON format
+    });
+});
+
+// create temperature and send back all temperatures after creation
+app.post('/api/temperatures', function(req, res) {
+
+    // create a temperature, information comes from AJAX request from Angular
+    Temperature.create({
+        _id : req.body.id,
+        Value : req.body.value,
+        Timestamp : req.body.timestamp,
+        done : false
+    }, function(err, temperature) {
+        if (err)
+            res.send(err);
+
+        // get and return all the todos after you create another
+        Temperature.find(function(err, temperatures) {
+            if (err)
+                res.send(err)
+            res.json(temperatures);
+        });
+    });
+
+});
+
+// delete a todo
+app.delete('/api/temperatures/:temperatures_id', function(req, res) {
+    Temperature.remove({
+        _id : req.params.temperatures_id
+    }, function(err, todo) {
+        if (err)
+            res.send(err);
+
+        // get and return all the todos after you create another
+        Temperature.find(function(err, temperatures) {
+            if (err)
+                res.send(err)
+            res.json(temperatures);
+        });
+    });
+});
+
+
+// listen (start app with node server.js)
+app.listen(8080);
+console.log("Server listening on port 8080");
---
+++ simulatoreSensore.py
@@ -0,0 +1,40 @@
+from pymongo import MongoClient 
+import numpy as np
+import pandas as pd 
+
+# constants
+MIN_TEMPERATURE = 18.0
+MAX_TEMPERATURE = 39.0
+TOTAL_OCCURRENCES = 20
+START_TIMESTAMP = "01-01-2018 08:30:00"
+END_TIMESTAMP = "31-10-2018 20:30:00"
+
+# simulation of sensor readings with arrays
+IDS = list(range(1,21))
+VALUES = np.random.uniform(low=MIN_TEMPERATURE, high=MAX_TEMPERATURE, size=(TOTAL_OCCURRENCES,))
+TIMESTAMPS = pd.date_range(start='2016-01-15', periods=20, freq='1M').strftime('%Y-%m-%d').flatten()
+
+# client db (localhost)
+client = MongoClient("mongodb://localhost:27017/")
+
+# connection to db
+db = client["zero12_db"]
+
+# object referring to the collection "temperatures"
+collectionTemperatures = db["temperatures"]
+
+# insertion of occurrences in the db
+for index in range(TOTAL_OCCURRENCES):
+	temp = {
+	    '_id': str(IDS[index]),
+	    'value': str(VALUES[index]),
+	    'timestamp': str(TIMESTAMPS[index])
+	}
+	result = collectionTemperatures.insert_one(temp)
+
+# reading the occurrences in the db
+for data in collectionTemperatures.find():
+	print(data)
+
+# elimination of the collection "temperatures" from the db
+#collectionTemperatures.drop()
